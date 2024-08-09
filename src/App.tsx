ca62368104ffffbb99bd2e4b017621831398d889
Kiet1122 <166615603+Kiet1122@users.noreply.github.com>
2024-08-09 20:46:24 +0700

    second code 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Navbar } from './layout/Navbar';
 import { Bodyab } from './layout/bodya';
